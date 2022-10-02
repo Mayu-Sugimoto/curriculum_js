daa1de0c803e08720f196c014ad87ff803a26df7
Mayu-Sugimoto <112380668+Mayu-Sugimoto@users.noreply.github.com>
2022-10-02 09:57:02 +0900

    Update main.js

--- sample_site1/js/main.js
+++ sample_site1/js/main.js
@@ -39,13 +39,13 @@ document.querySelector('.link-3').addEventListener('click', function(){
 
 $('.slider').slick({
   arrows: false,//左右の矢印はなし
-  autoplay: true,//自動的に動き出すか。初期値はfalse。
-  autoplaySpeed: 0,//自動的に動き出す待ち時間。初期値は3000ですが今回の見せ方では0
-  speed: 6900,//スライドのスピード。初期値は300。
-  infinite: true,//スライドをループさせるかどうか。初期値はtrue。
-  pauseOnHover: false,//オンマウスでスライドを一時停止させるかどうか。初期値はtrue。
-  pauseOnFocus: false,//フォーカスした際にスライドを一時停止させるかどうか。初期値はtrue。
-  cssEase: 'linear',//動き方。初期値はeaseですが、スムースな動きで見せたいのでlinear
+  autoplay: true,//自動的に動き出すか
+  autoplaySpeed: 0,//自動的に動き出す待ち時間
+  speed: 6900,//スライドのスピード
+  infinite: true,//スライドをループさせるかどうか
+  pauseOnHover: false,//オンマウスでスライドを一時停止させるかどうか
+  pauseOnFocus: false,//フォーカスした際にスライドを一時停止させるかどうか
+  cssEase: 'linear',//動き方。スムースな動きで見せたいのでlinear
   slidesToShow: 4,//スライドを画面に4枚見せる
   slidesToScroll: 1,//1回のスライドで動かす要素数
   responsive: [
